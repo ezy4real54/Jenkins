892f39df644c14f964cc98a747170d39708c5813
ERONDU Ezinne Chinwe <46625702+ezy4real54@users.noreply.github.com>
2022-07-12 11:43:08 +0200

    Update calc.py

--- sources/calc.py
+++ sources/calc.py
@@ -1,7 +1,20 @@
-a = 1
-b = 2
-c = -4
+def add(x, y):
+    """Add Function"""
+    return x + y
 
-delta = b ** 2 - 4 * a * c
-print('Le discriminant est :')
-print(delta)
+
+def subtract(x, y):
+    """Subtract Function"""
+    return x - y
+
+
+def multiply(x, y):
+    """Multiply Function"""
+    return x * y
+
+
+def divide(x, y):
+    """Divide Function"""
+    if y == 0:
+        raise ValueError('Can not divide by zero!')
+    return x / y
